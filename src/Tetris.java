a398bce78ca3d0ab1c1cc80fed2a4f041f207789
ejramirez5 <43324095+ejramirez5@users.noreply.github.com>
2019-07-28 15:00:35 -0600

    Add files via upload

--- src/Tetris.java
+++ src/Tetris.java
@@ -1,19 +1,35 @@
+package serverSS;
 import controller.*;
 import model.*;
 import view.*;
 import view.animation.*;
+
 
 public class Tetris {
     public static void main(String[] args){
 //        Operations();
 //    }
 //    public static void Operations() {
-    	Controller Jem = new Controller();
-    	Jem.init();
+    	Controller Jem = new Controller(); // change this var name
     	Jem.curUi = new TetrisUI(args);
-    	(Jem.curUi).addKeyListener(new Controller());
+    	Jem.init();
+
+    	(Jem.curUi).addKeyListener(Jem);
     	(Jem.curUi).setFocusable(true);
-    	Jem.curUi.run();    			
+    	Jem.curUi.requestFocusInWindow();
+
+    	Jem.curUi.run();    			    	
+    
+    	
+//    	Control Jem;
+//    	Jem = new Control(args);
+//    	Jem.init();
+//    	Jem.addKeyListener(Jem);
+//    	Jem.setFocusable(true);
+//    	Jem.requestFocusInWindow();
+//    	Jem.run();
+    	
+    	
     	
     	
     }
